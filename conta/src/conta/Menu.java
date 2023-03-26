cf6cdbb40b994728b2741be538ac6dee80f93b20
ChristianBonetti <christian_bonetti@hotmail.com>
2023-03-26 12:33:01 -0300

    projetov2

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -1,9 +1,34 @@
 package conta;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+int numero,somaPar=0,contImpar=0;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("\nEntre com o numero");
+		numero = leia.nextInt();
+		
+		while(numero != 0) {
+			
+			
+			if(numero % 2 == 0) {
+				somaPar += numero;
+			} else {
+				contImpar++;
+				}
+			System.out.println("\nEntre com um número");
+			numero = leia.nextInt();
+		}	
+		
+		System.out.println("\nSomatório dos números pares foi de:"+somaPar);
+		System.out.println("\nQuantidade de números ímpares foi de:"+contImpar);
+		
+		
+		
 
 	}
 
